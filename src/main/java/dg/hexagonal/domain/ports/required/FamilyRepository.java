40d48a28305f7bbc8e8056627f50f57a4c418638
Dominika Grebowiec <dominika.grebowiec@gmail.com>
2017-06-15 10:02:03 +0200

    Remove unused method.

--- src/main/java/dg/hexagonal/domain/ports/required/FamilyRepository.java
+++ src/main/java/dg/hexagonal/domain/ports/required/FamilyRepository.java
@@ -2,13 +2,10 @@ package dg.hexagonal.domain.ports.required;
 
 import dg.hexagonal.domain.Event;
 import dg.hexagonal.domain.Family;
-import dg.hexagonal.domain.FamilyMember;
 
 public interface FamilyRepository {
 
 	public void addFamily(Family family);
-
-	public Family addKidToFamily(Long familyId, FamilyMember kid);
 
 	public void updateFamily(Family family);
 
